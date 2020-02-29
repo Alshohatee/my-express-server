e63e9c0bc6df9482e2fd10019b5b18ab371f177b
alshohatee <alshohateeaseel@gmail.com>
2020-02-29 16:14:27 -0500

    notes about html been send the browser using app.send('<h1></h1>')

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 // importing express so we can use it here
 const express = require("express");
 
-// function that represent a function from the express
+// function that represent a express function
 const app = express();
+
+
+app.get("/", function (req, res) {
+    //this is the home or the root page
+    console.log(res)
+    console.log(req)
+
+    res.send(
+        "we can send text through this function" + "<h1>sending html</h1>"
+    )
+})
+
 
 
 
